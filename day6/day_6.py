1025a4aac066e41837c2279b96d251df56bdc8b1
mdmake <mmake.md@gmail.com>
2022-12-06 10:09:54 +0300

    day 6 -- update

--- day6/day_6.py
+++ day6/day_6.py
@@ -1,23 +1,19 @@
 from collections import deque
 
 
-def partOne(filename):
+def puzzle(filename: str, lenght: int) -> int:
     with open(filename) as f:
         data = f.readline().strip()
 
-    buffer = deque(maxlen=14)
+    buffer = deque(maxlen=lenght)
     for i, ch in enumerate(data):
         buffer.append(ch)
-        if len(set(buffer)) == 14:
-            return i+1
+        if len(set(buffer)) == lenght:
+            return i + 1
 
     return -1
 
 
-def partTwo(filename):
-    pass
-
-
 if __name__ == '__main__':
-    print(partOne('input.txt'))
-    #print(partTwo('input.txt'))
+    print(puzzle('input.txt', 4))
+    print(puzzle('input.txt', 14))
