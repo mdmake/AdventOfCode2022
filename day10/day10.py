2d4addbf88e4062c3b1284f632003d4f83ed7b3a
mdmake <mmake.md@gmail.com>
2022-12-10 09:15:59 +0300

    day 10 - update

--- day10/day10.py
+++ day10/day10.py
@@ -2,81 +2,69 @@ def partOne(filename: str):
     x = 1
     cycle = 0
     rez = 0
+
+    ticks = {20, 60, 100, 140, 180, 220}
     with open(filename) as f:
         for line in f:
 
             if 'noop' in line:
                 cycle += 1
-                if cycle in [20, 60, 100, 140, 180, 220]:
+                if cycle in ticks:
                     rez += cycle * x
-                    print(cycle, x)
 
             else:
                 _, var = line.strip().split()
+
                 cycle += 1
-                if cycle in [20, 60, 100, 140, 180, 220]:
+                if cycle in ticks:
                     rez += cycle * x
-                    print(cycle, x)
+
                 cycle += 1
-                if cycle in [20, 60, 100, 140, 180, 220]:
+                if cycle in ticks:
                     rez += cycle * x
-                    print(cycle, x)
 
                 x += int(var)
 
-    if cycle in [20, 60, 100, 140, 180, 220]:
-        rez += cycle * x
-        print(cycle, x)
-
-    return x, cycle, rez
+    print(rez)
 
 
 def partTwo(filename: str):
+    LEN = 40
     x = 1
     cycle = 0
-    rez = 0
-    baseline = ['.'] * 40
+    baseline = ['.'] * LEN
     newline = baseline.copy()
     with open(filename) as f:
         for line in f:
             if 'noop' in line:
 
-                if abs(cycle % 40 - x) < 2:
-                    newline[cycle % 40] = '#'
+                if abs(cycle % LEN - x) < 2:
+                    newline[cycle % LEN] = '#'
                 cycle += 1
-
-                if cycle % 40 == 0:
+                if cycle % LEN == 0:
                     print(''.join(newline))
                     newline = baseline.copy()
 
             else:
                 _, var = line.strip().split()
-                if abs(cycle % 40 - x) < 2:
-                    newline[cycle % 40] = '#'
-                cycle += 1
 
-                if cycle % 40 == 0:
+                if abs(cycle % LEN - x) < 2:
+                    newline[cycle % LEN] = '#'
+                cycle += 1
+                if cycle % LEN == 0:
                     print(''.join(newline))
                     newline = baseline.copy()
 
-
-                if abs(cycle % 40 - x) < 2:
-                    newline[cycle % 40] = '#'
+                if abs(cycle % LEN - x) < 2:
+                    newline[cycle % LEN] = '#'
                 cycle += 1
-
-                if cycle % 40 == 0:
+                if cycle % LEN == 0:
                     print(''.join(newline))
                     newline = baseline.copy()
 
                 x += int(var)
 
-    if cycle in [20, 60, 100, 140, 180, 220]:
-        rez += cycle * x
-        print(cycle, x)
-
-    return x, cycle, rez
-
 
 if __name__ == '__main__':
-    print(partOne('input.txt'))
-    print(partTwo('input.txt'))
+    partOne('input.txt')
+    partTwo('input.txt')
